fba2a23eb00e33ed308c0a22b3040519e4bd238b
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2024-02-16 00:12:59 +0530

    Added authentication with Supabase

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,127 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+'use client';
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as z from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+import { FormSchema } from '@/lib/types';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import Logo from '../../../../public/scribelogo.svg';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Loader from '@/components/loader';
+import { actionLoginUser } from '@/lib/server-action/auth-actions';
 
 const LoginPage = () => {
+    const router = useRouter();
+    const [submitError, setSubmitError] = useState('');
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        mode: 'onChange',
+        resolver: zodResolver(FormSchema),
+        defaultValues: {email: '', password: ''},
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit:SubmitHandler<z.infer<typeof FormSchema>> = async (
+        formData
+    ) => {
+        const {error} = await actionLoginUser(formData);
+        if(error) {
+            form.reset();
+            setSubmitError(error.message);
+        }
+        router.replace('/dashboard');
+    };
   return (
-    <div>LoginPage</div>
+    <Form {...form}>
+        <form 
+          onChange={() => {
+            if(submitError) setSubmitError('');
+          }}
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col'
+        >
+            <Link 
+              href="/"
+              className='w-full 
+              flex
+              justify-left
+              items-center'
+            > 
+                <Image 
+                  src={Logo} 
+                  alt="scribe logo" 
+                  width={50} 
+                  height={50}
+                />
+                <span className='font-semibold
+                dark:text-white text-4xl ml-2'
+                > 
+                    scribe.
+                </span>
+            </Link>    
+            <FormDescription className='
+              text-foreground/60'
+            >
+                An all-in-one Collaboration and Productivity Platform
+            </FormDescription>
+            <FormField 
+              disabled={isLoading} 
+              control={form.control}
+              name="email"
+              render={(field) => (
+                <FormItem>
+                    <FormControl>
+                        <Input 
+                          type="email" 
+                          placeholder='Email'
+                          {...field}
+                        />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+              )}
+            />
+            
+            <FormField 
+              disabled={isLoading} 
+              control={form.control}
+              name="password"
+              render={(field) => (
+                <FormItem>
+                    <FormControl>
+                        <Input 
+                          type="password" 
+                          placeholder='Password'
+                          {...field}
+                        />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+              )}
+            />
+            {submitError && <FormMessage>{submitError}</FormMessage>}
+            <Button
+              type="submit"
+              className='w-full p-6'
+              size="lg"
+              disabled={isLoading}
+            >
+                {!isLoading ? "Login" : <Loader/>}
+            </Button>
+            <span className='self-container'>
+                Dont have an account?
+                <Link href="/signup"
+                 className='text-primary'
+                > Sign Up
+                </Link>
+            </span>
+        </form>
+    </Form>
   )
 }
 
