145e7e57c6acb4b9b879797ad8c4287a8895f25e
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2024-04-03 15:54:42 +0530

    Added useEffects for data sync

--- src/components/quill-editor/quill-editor.tsx
+++ src/components/quill-editor/quill-editor.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { useAppState } from "@/lib/providers/state-provider";
 import { File, Folder, workspace } from "@/lib/supabase/supabase.types";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import "quill/dist/quill.snow.css";
 import { Button } from "../ui/button";
 import {
@@ -29,6 +35,7 @@ import EmojiPicker from "../global/emoji-picker";
 import BannerUpload from "../banner-upload/banner-upload";
 import { XCircleIcon } from "lucide-react";
 import { useSocket } from "@/lib/providers/socket-provider";
+import { useSupabaseUser } from "@/lib/providers/supabase-user-provider";
 
 interface QuillEditorProps {
   dirDetails: File | Folder | workspace;
@@ -63,6 +70,8 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
 }) => {
   const supabase = createClientComponentClient();
   const { state, workspaceId, folderId, dispatch } = useAppState();
+  const saveTimerRef = useRef<ReturnType<typeof setTimeout>>();
+  const { user } = useSupabaseUser();
   const router = useRouter();
   const { socket } = useSocket();
   const pathname = usePathname();
@@ -352,6 +361,72 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
     if (socket === null || quill === null || !fileId) return;
     socket.emit("create-room", fileId);
   }, [socket, quill, fileId]);
+
+  //send quill changes to all clients
+  useEffect(() => {
+    if (quill === null || socket === null || !fileId || !user) return;
+    //WIP cursors update
+    const selectionChangeHandler = () => {};
+
+    const quillHandler = (delta: any, oldDelta: any, source: any) => {
+      if (source !== "user") return;
+      if (saveTimerRef.current) clearTimeout(saveTimerRef.current);
+      setSaving(true);
+      const contents = quill.getContents();
+      const quillLength = quill.getLength();
+      saveTimerRef.current = setTimeout(async () => {
+        if (contents && quillLength !== 1 && fileId) {
+          if (dirType === "workspace") {
+            dispatch({
+              type: "UPDATE_WORKSPACE",
+              payload: {
+                workspace: { data: JSON.stringify(contents) },
+                workspaceId: fileId,
+              },
+            });
+            await updateWorkspace({ data: JSON.stringify(contents) }, fileId);
+          }
+
+          if (dirType === "folder") {
+            if (!workspaceId) return;
+            dispatch({
+              type: "UPDATE_FOLDER",
+              payload: {
+                folder: { data: JSON.stringify(contents) },
+                workspaceId: fileId,
+                folderId: fileId,
+              },
+            });
+            await updateFolder({ data: JSON.stringify(contents) }, fileId);
+          }
+
+          if (dirType === "file") {
+            if (!workspaceId || !folderId) return;
+            dispatch({
+              type: "UPDATE_FILE",
+              payload: {
+                file: { data: JSON.stringify(contents) },
+                workspaceId,
+                folderId: folderId,
+                fileId,
+              },
+            });
+            await updateFile({ data: JSON.stringify(contents) }, fileId);
+          }
+        }
+        setSaving(false);
+      }, 850);
+      socket.emit("send-changes", delta, fileId);
+    };
+    quill.on("text-change", quillHandler);
+    //WIP cursors selection handler
+
+    return () => {
+      quill.off("text-change", quillHandler);
+      //WIP cursors
+      if (saveTimerRef.current) clearTimeout(saveTimerRef.current);
+    };
+  }, [quill, socket, fileId, user, details]);
 
   return (
     <>
